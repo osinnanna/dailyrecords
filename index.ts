b2533beea5205e2c74cd0bd308f01888ff991e28
Nnanna Osinachukwu <contact2amondz24@gmail.com>
2026-01-06 22:40:05 +0100

    feat: serve html page

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import { serve } from "bun";
+import homepage from "./src/frontend/index.html";
+
+const server = serve({
+    port: 3000,
+    routes: {
+        "/": homepage,
+    },
+    development: true,
+});
+
+console.log(`Server is running at ${server.url}`);
