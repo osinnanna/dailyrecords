f915d8445e28740302147b0f5b139ccbd76fd75b
Nnanna Osinachukwu <contact2amondz24@gmail.com>
2026-01-08 19:08:44 +0100

    feat: more styling
    Its still ugly, -> adding an optionaly timestamp input

---
+++ src/testingjs/Date.ts
@@ -0,0 +1,6 @@
+let date: string = "2026-12-04 2:00:00";
+const hello = new Date(date).toLocaleString();
+
+console.log(hello);
+
+// This is to know how the user would be able to insertData in the case that the PI is off or something went wrong with the localAreaNetwork.
